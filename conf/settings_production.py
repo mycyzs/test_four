c07780a2c7cf4e3adc4953966874f881887cae40
mycyzs <mycyzs@163.com>
2019-06-29 13:10:39 +0800

    daak

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -13,7 +13,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'YtX#_]0[6n',                        # 你的数据库password
+        'PASSWORD': 'YtX#_]O[6n',                        # 你的数据库password
         'HOST': '192.168.163.52',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
